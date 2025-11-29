f39a1d0805e14a683fa19ed8c14ff5a19b69664e
Jayden Tillery <jbtillery01@gmail.com>
2025-11-29 03:52:03 -0500

    Successful Response Object Created

--- pull_aq.py
+++ pull_aq.py
@@ -1,7 +1,20 @@
 import requests
+from dotenv import load_dotenv
+import os
+from pprint import pprint
 
-data = requests.get("https://api.openaq.org/v2/locations")
+load_dotenv() # Checks 
+
+key = os.getenv("aq")
+
+header = {"X-API-Key":key}
+
+data = requests.get("https://api.openaq.org/v3/locations",headers=header)
+
 data.raise_for_status()
+
 locations = data.json()
+
 print("Successful response!!")
 
+pprint(locations)
